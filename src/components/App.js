3815fb482f0b449a94c6a1e51468bde73c4cf4e3
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-14 14:02:55 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
+  const [todos,setTodo]=useState([{item:"Learn React",state1:false},{item:"Build a React app",state1:false},{item:"Deploy the React app ",state1:false}])
+  function handleComplete(item){
+    setTodo(todos.map((element)=>{
+      if(element.item==item){
+        return{... element,state1:true}
+        
+      }
+      return element;
+    })
+  
+  
+ )
+    
+
+
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const TodoList = (props) => {
+  return (
+    <div>
+       <ul>
+     {
+        props.todos.map(element => {
+
+       return <li>
+
+             <span>{element.item}</span>{element.state1==false?<button onClick={()=>props.handleComplete(element.item)}>Complete</button>:""}
+
+
+          </li>
+
+            
+        })
+     }
+       </ul>
+    </div>
+  )
+}
+
+export default TodoList
